0cea829c99fc592dfa5ba963df7361a7c9d53073
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-09-20 00:35:51 +0000

    register jwt strategy

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
+import * as config from 'config'
+
+const jwtConfig = config.get('jwt');
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: jwtConfig.secret,
+      signOptions: {
+        expiresIn: jwtConfig.expiresIn,
+      }
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
