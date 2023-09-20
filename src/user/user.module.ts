5511bf8eff7033bb41dbcc20102e12347e60809f
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-09-20 00:37:48 +0000

    add JwtStrategy and PassportModule to exports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,6 +6,9 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
 import * as config from 'config'
+import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 const jwtConfig = config.get('jwt');
 @Module({
@@ -19,8 +22,10 @@ const jwtConfig = config.get('jwt');
         expiresIn: jwtConfig.expiresIn,
       }
     }),
+    TypeOrmModule.forFeature([User])
   ],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService,JwtStrategy, UserRepository],
+  exports: [PassportModule, JwtStrategy]
 })
 export class UserModule {}
