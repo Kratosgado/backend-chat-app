a4fa74771c4a2ce07fc254579967cf1bd3621315
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-10-18 10:14:14 +0000

    add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserService } from './user.service';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   providers: [PrismaService, UserService],
   controllers: [AppController],
+  imports: [UserModule],
 })
 export class AppModule {}
